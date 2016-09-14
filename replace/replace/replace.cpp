7985e99167895a58863e35db518f5fbe96b6a226
AnnVita <kublitskayavd@yandex.ru>
2016-09-14 11:15:38 +0400

    Проверка на наличие всех аругментов и наличия всех файлов

--- replace/replace/replace.cpp
+++ replace/replace/replace.cpp
@@ -1,8 +1,3 @@
-/* replace.cpp : выполняет замену подстроки в текстовом файле на другую строку,
-и записывает результат в выходной файл (отличный от входного).
-Формат коммандной строки: replace.exe <input file><output file><search string><replace string>
-*/
-
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
@@ -14,7 +9,23 @@ int main(int argc, char * argv[])
 	if (argc != 3)
 	{
 		cout << "Invalid arguments count\n"
-			 << "replace.exe <input file><output file><search string><replace string>\n";
+			<< "Usage: copyfile.exe <input file> <output file>\n";
+		return 1;
+	}
+
+	ifstream input(argv[1]);
+
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+	}
+
+	ofstream output(argv[2]);
+
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
 		return 1;
 	}
 
