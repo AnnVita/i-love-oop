5f2e23e4bcde7809b0e9fb8eefbe3ba7e09032fd
AnnVita <kublitskayavd@yandex.ru>
2016-09-22 10:04:16 +0400

    Интерфейс программы, bool функции контроля переполнения

--- radix/radix/radix.cpp
+++ radix/radix/radix.cpp
@@ -2,13 +2,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <math.h> 
+
+static const int DEC_RADIX = 10;
+static const int MAX_RADIX = 36;
+static const int MIN_RADIX = 2;
 
 using namespace std;
 
 int CharToInt(char const &ch);
 bool CheckInputRadix(int const &radix);
 int StringToInt(const string &str, int radix, bool &wasError);
+
+void StringIntToString(int n, int radix, bool &wasError);
+bool OwerflowAtAddition(int const &addendum1, int const &addendum2);
+bool OwerflowAtMultiplication(int const &multiplier1, int const &multiplier2);
+int Pow(int const &base, int const &pow, bool &wasError);
+int TransferDecimalToAnother(int const &decimal, int const &destinationRadix);
 
 int main(int argc, char * argv[])
 {
@@ -45,19 +54,30 @@ int CharToInt(char const &ch)
 
 bool CheckInputRadix(int const &radix)
 {
-	return ((radix >= 2) && (radix <= 36));
+	return ((radix >= MIN_RADIX) && (radix <= MAX_RADIX));
+}
+
+bool OwerflowAtAddition(int const &addendum1, int const &addendum2)
+{
+	return ((addendum1 > 0) && (addendum2 > 0) && (addendum1 > (INT_MAX - addendum2)));
+}
+
+bool OwerflowAtMultiplication(int const &multiplier1, int const &multiplier2)
+{
+	return (multiplier1 >= INT_MAX / multiplier2);
 }
 
 int StringToInt(const string &str, int radix, bool &wasError)
 {
-	bool isNegate = (str[0] == '-');
-	int i = (isNegate ? 1 : 0),
+	bool isNegative = (str[0] == '-');
+	int i = (isNegative ? 1 : 0),
 		result = 0,
 		digit, digitIndex;
 	for (digitIndex = 0; i <= str.length(); i++, digitIndex++)
 	{
-		digit = CharToInt(str[i]) * int(pow(radix, digitIndex));
-		if (!((digit > 0) && (result > (INT_MAX - digit)))
+		digit = CharToInt(str[i]);
+		digit *= Pow(radix, digitIndex);
+		if (!)
 		{
 			result += digit;
 		}
