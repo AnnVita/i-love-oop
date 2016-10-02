b5f890021ea99d8d6fc351df0add59ed435326dc
AnnVita <kublitskayavd@yandex.ru>
2016-10-02 19:19:14 +0400

    Перевод строки в decimal int
    Переводит введенную строку в десятичное число, в случае ошибки
    возвращает меняет wasError на true

--- radix/radix/radix.cpp
+++ radix/radix/radix.cpp
@@ -10,14 +10,15 @@ static const int MIN_RADIX = 2;
 using namespace std;
 
 int CharToInt(char const &ch);
-bool CheckInputRadix(int const &radix);
+bool CorrectInputRadix(int const &radix);
 int StringToInt(const string &str, int radix, bool &wasError);
 
-void StringIntToString(int n, int radix, bool &wasError);
-bool OwerflowAtAddition(int const &addendum1, int const &addendum2);
-bool OwerflowAtMultiplication(int const &multiplier1, int const &multiplier2);
-int Pow(int const &base, int const &pow, bool &wasError);
-int TransferDecimalToAnother(int const &decimal, int const &destinationRadix);
+//void StringIntToString(int n, int radix, bool &wasError);
+bool OverflowAtAddition(int const &addendum1, int const &addendum2);
+bool OverflowAtMultiplication(int const &multiplier1, int const &multiplier2);
+bool DigitValueBelongToRadix(int const &digitValue, int const &radix);
+int Pow(int const &base, int const &exponent, bool &wasError);
+//int TransferDecimalToAnother(int const &decimal, int const &destinationRadix);
 
 int main(int argc, char * argv[])
 {
@@ -28,11 +29,20 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	int sourceNotation = stoi(argv[1]),
+	int result,
+		sourceNotation = stoi(argv[1]),
 		destinationNotation = stoi(argv[2]);
-	string numValue;
+	string numValue = argv[3];
+	bool wasError = false;
 
+	if (!CorrectInputRadix(sourceNotation) || !CorrectInputRadix(destinationNotation))
+	{
+		cout << "Invalid notation values\n"
+			 << "Values must be in the interval from " << MIN_RADIX << " to " << MAX_RADIX <<"\n";
+		return 1;
+	}
 
+	result = StringToInt(numValue, sourceNotation, wasError);
 	return 0;
 }
 
@@ -52,34 +62,57 @@ int CharToInt(char const &ch)
 	return number;
 }
 
-bool CheckInputRadix(int const &radix)
+bool CorrectInputRadix(int const &radix)
 {
 	return ((radix >= MIN_RADIX) && (radix <= MAX_RADIX));
 }
 
-bool OwerflowAtAddition(int const &addendum1, int const &addendum2)
+bool OverflowAtAddition(int const &addendum1, int const &addendum2)
 {
 	return ((addendum1 > 0) && (addendum2 > 0) && (addendum1 > (INT_MAX - addendum2)));
 }
 
-bool OwerflowAtMultiplication(int const &multiplier1, int const &multiplier2)
+bool OverflowAtMultiplication(int const &multiplier1, int const &multiplier2)
 {
 	return (multiplier1 >= INT_MAX / multiplier2);
+}
+
+int Pow(int const &base, int const &exponent, bool &wasError)
+{
+	int i, result = 1;
+	for (i = exponent; i > 0; i--)
+	{
+		if (OverflowAtMultiplication(result, base))
+		{
+			result = -1;
+			wasError = true;
+			break;
+		}
+		else
+			result *= base;
+	}
+	return result;
+}
+
+bool DigitValueBelongToRadix(int const &digitValue, int const &radix)
+{
+	return (digitValue < radix) && (digitValue >= 0);
 }
 
 int StringToInt(const string &str, int radix, bool &wasError)
 {
 	bool isNegative = (str[0] == '-');
-	int i = (isNegative ? 1 : 0),
-		result = 0,
-		digit, digitIndex;
-	for (digitIndex = 0; i <= str.length(); i++, digitIndex++)
+	int i, digit, digitIndex, powOfTheRadix,
+		result = 0;
+	for (digitIndex = 0, i = str.length() - 1; (i >= (int)isNegative) && (!wasError); i--, digitIndex++)
 	{
 		digit = CharToInt(str[i]);
-		digit *= Pow(radix, digitIndex);
-		if (!)
-		{
-			result += digit;
-		}
+		wasError = !DigitValueBelongToRadix(digit, radix);
+		powOfTheRadix = Pow(radix, digitIndex, wasError);
+		wasError = (wasError) || (OverflowAtMultiplication(digit, powOfTheRadix));
+		digit = (!wasError) ? digit * powOfTheRadix : -1 ;
+		wasError = (wasError) || (OverflowAtAddition(result, digit));
+		result = (!wasError) ? result + digit: -1 ;
 	}
+	return result;
 }
