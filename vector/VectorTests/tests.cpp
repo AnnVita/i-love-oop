4691cae4d939b702c92a7ee506de45ac884e9e40
AnnVita <kublitskayavd@yandex.ru>
2016-10-19 11:37:35 +0400

    Добавлены тесты для vector

--- vector/VectorTests/tests.cpp
+++ vector/VectorTests/tests.cpp
@@ -3,6 +3,9 @@
 
 namespace
 {
+	DoubleVector emptyVector = {};
+	DoubleVector singleElementVector = {2};
+	DoubleVector testVectorWithValues = { 9, 1, -2, -10, 0, 9, 5, -11 };
 	void VerifyGetAverageOfPositiveElements(DoubleVector & vector, double &expectedValue)
 	{
 		BOOST_CHECK(GetAverageOfPositiveElements(vector) == expectedValue);
@@ -18,20 +21,30 @@ namespace
 BOOST_AUTO_TEST_SUITE(GetAverageOfPositiveElements_function)
 BOOST_AUTO_TEST_CASE(must_count_average_of_all_positive_values_in_vector)
 {
-
-	DoubleVector testVector = { 9, 1, -2, -10, 0, 9, 5, -11 };
 	double expectedValue = 6;
-	VerifyGetAverageOfPositiveElements(testVector, expectedValue);
+	VerifyGetAverageOfPositiveElements(testVectorWithValues, expectedValue);
 }
+
+BOOST_AUTO_TEST_CASE(must_return_zero_if_function_dont_found_positive_numbers)
+{
+	double expectedValue = 0;
+	VerifyGetAverageOfPositiveElements(emptyVector, expectedValue);
+}
+
+BOOST_AUTO_TEST_CASE(must_return_value_of_element_if_in_vector_only_one_element)
+{
+	double expectedValue = 2;
+	VerifyGetAverageOfPositiveElements(singleElementVector, expectedValue);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(AddToAllElements_function)
 BOOST_AUTO_TEST_CASE(must_add_to_all_vector_elements_the_same_value)
 {
-
-	DoubleVector testVector = { 9, 1, -2, -10, 0, 9, 5, -11 };
 	DoubleVector expectedVector = { 11, 3, 0, -8, 2, 11, 7, -9 };
 	double valToAdd = 2;
-	VerifyAddToAllElements(testVector, valToAdd, expectedVector);
+	VerifyAddToAllElements(testVectorWithValues, valToAdd, expectedVector);
 }
+
 BOOST_AUTO_TEST_SUITE_END()
