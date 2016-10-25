fa1e8619e83a160870a805ee7f2d129589847b36
AnnVita <kublitskayavd@yandex.ru>
2016-10-26 00:11:11 +0400

    Поправлены тесты к FindAndReplace, инициализация dictionary

--- FindAndReplace/Test/FIndAndReplaceTest.cpp
+++ FindAndReplace/Test/FIndAndReplaceTest.cpp
@@ -10,28 +10,28 @@ namespace
 }
 
 BOOST_AUTO_TEST_SUITE(FindAndReplace_function)
-BOOST_AUTO_TEST_CASE(must_convert_empty_string_to_empty_string)
-{
-	VerifyFindAndReplace("", "", "", "");
-}
+    BOOST_AUTO_TEST_CASE(must_convert_empty_string_to_empty_string)
+    {
+	    VerifyFindAndReplace("", "", "", "");
+    }
 
-BOOST_AUTO_TEST_CASE(if_in_input_an_empty_string_must_return_empty_string)
-{
-	VerifyFindAndReplace("", "smth", "empty", "");
-}
+	BOOST_AUTO_TEST_CASE(if_in_input_an_empty_string_must_return_empty_string)
+	{
+		VerifyFindAndReplace("", "smth", "empty", "");
+	}
 
-BOOST_AUTO_TEST_CASE(must_replace_word_by_empty_string)
-{
-	VerifyFindAndReplace("input string", "string", "", "input ");
-}
+	BOOST_AUTO_TEST_CASE(must_replace_word_by_empty_string)
+	{
+		VerifyFindAndReplace("input string", "string", "", "input ");
+	}
 
-BOOST_AUTO_TEST_CASE(must_replace_symbol_by_another_symbol)
-{
-	VerifyFindAndReplace("input string", "i", "l", "lnput strlng");
-}
+	BOOST_AUTO_TEST_CASE(must_replace_symbol_by_another_symbol)
+	{
+		VerifyFindAndReplace("input string", "i", "l", "lnput strlng");
+	}
 
-BOOST_AUTO_TEST_CASE(must_replace_substring_by_another_string)
-{
-	VerifyFindAndReplace("input string", "input", "output", "output string");
-}
+	BOOST_AUTO_TEST_CASE(must_replace_substring_by_another_string)
+	{
+		VerifyFindAndReplace("input string", "input", "output", "output string");
+	}
 BOOST_AUTO_TEST_SUITE_END()
