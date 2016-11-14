0bfd5cc815fe1502089aadf348afb1939c0c69ac
AnnVita <kublitskayavd@yandex.ru>
2016-11-14 23:38:23 +0300

    Реализация

--- Car/Car/car.cpp
+++ Car/Car/car.cpp
@@ -51,6 +51,50 @@ bool CCar::TurnOffEngine()
 	return false;
 }
 
+bool CCar::SetSpeed(int speed)
+{
+	if (m_engineOn && IsSpeedInRange(speed, m_gear) && !((m_gear == Gear::NEUTRAL) && (speed >= m_speed)))
+	{
+		if (speed == 0)
+		{
+			m_direction = Direction::NONE;
+		}
+		else if (m_gear == Gear::REVERSE)
+		{
+			m_direction = Direction::BACKWARD;
+		}
+		else
+		{
+			m_direction = Direction::FORWARD;
+		}
+		m_speed = speed;
+		return true;
+	}
+	return false;
+}
+
+bool CCar::SetGear(Gear newGear)
+{
+	if (!m_engineOn || !IsSpeedInRange(m_speed, newGear) || !IsDirectionCorrect(m_direction, newGear))
+		return false;
+
+	if (newGear == Gear::REVERSE)
+	{
+		if (m_gear > Gear::FIRST || m_speed != 0)
+			return false;
+	}
+
+	if (newGear == Gear::FIRST && m_gear < Gear::FIRST)
+	{
+		if (m_speed != 0)
+			return false;
+	}
+
+	m_gear = newGear;
+	return true;
+}
+
+
 bool CCar::IsSpeedInRange(int speed, Gear gear) const
 {
 	bool isInRange;
@@ -60,7 +104,7 @@ bool CCar::IsSpeedInRange(int speed, Gear gear) const
 		isInRange = ((speed >= 0) && (speed <= 20));
 		break;
 	case Gear::NEUTRAL:
-		isInRange = true;
+		isInRange = (speed >= 0);
 		break;
 	case Gear::FIRST:
 		isInRange = ((speed >= 0) && (speed <= 30));
@@ -78,9 +122,10 @@ bool CCar::IsSpeedInRange(int speed, Gear gear) const
 		isInRange = ((speed >= 50) && (speed <= 150));
 		break;
 	}
+	return isInRange;
 }
 
 bool CCar::IsDirectionCorrect(Direction direction, Gear gear) const
 {
-
+	return !(((direction == Direction::FORWARD) && (gear == Gear::REVERSE)) || ((direction == Direction::BACKWARD) && (gear > Gear::NEUTRAL)));
 }
