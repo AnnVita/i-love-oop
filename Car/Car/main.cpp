f0b79d02835913f4bb05d10a7bc99f0aa2ae11ba
AnnVita <kublitskayavd@yandex.ru>
2016-11-15 13:48:07 +0300

    Основная программа

--- Car/Car/main.cpp
+++ Car/Car/main.cpp
@@ -1,9 +1,22 @@
 #include "stdafx.h"
 #include "car.h"
+#include "carController.h"
 #include <iostream>
 
 int main()
 {
-    return 0;
+	CCar car;
+	CCarControl carController(car, std::cin, std::cout);
+
+	while ((!std::cin.eof()) && (!std::cin.fail()))
+	{
+		std::cout << "> ";
+		if (!carController.HandleCommand())
+		{
+			std::cout << "Unknown command!" << std::endl;
+		}
+	}
+
+	return 0;
 }
 
