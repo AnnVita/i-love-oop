9c857f2f3e475ee7f36c97560f3b9df9a194b53e
AnnVita <kublitskayavd@yandex.ru>
2016-11-16 09:38:53 +0300

    Поправлены названия тестов, выделен блок с проверкой значений класса по умолчанию

--- Car/tests/carTests.cpp
+++ Car/tests/carTests.cpp
@@ -8,25 +8,29 @@ struct CarFixture
 
 BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 
-	BOOST_AUTO_TEST_CASE(car_engine_is_turned_off_by_default)
-	{
-		BOOST_CHECK(!car.IsTurnedOn());
-	}
+    BOOST_AUTO_TEST_SUITE(when_the_newly_constructed)
 
-	BOOST_AUTO_TEST_CASE(car_speed_is_zero_by_default)
-	{
-		BOOST_CHECK(car.GetSpeed() == 0);
-	}
+		BOOST_AUTO_TEST_CASE(engine_is_turned_off_by_default)
+		{
+			BOOST_CHECK(!car.IsTurnedOn());
+		}
 
-	BOOST_AUTO_TEST_CASE(car_direction_is_NONE_by_default)
-	{
-		BOOST_CHECK(car.GetDirection() == Direction::NONE);
-	}
+		BOOST_AUTO_TEST_CASE(speed_is_zero_by_default)
+		{
+			BOOST_CHECK(car.GetSpeed() == 0);
+		}
 
-	BOOST_AUTO_TEST_CASE(car_gear_is_NEUTRAL_by_default)
-	{
-		BOOST_CHECK(car.GetGear() == Gear::NEUTRAL);
-	}
+		BOOST_AUTO_TEST_CASE(direction_is_NONE_by_default)
+		{
+			BOOST_CHECK(car.GetDirection() == Direction::NONE);
+		}
+
+		BOOST_AUTO_TEST_CASE(gear_is_NEUTRAL_by_default)
+		{
+			BOOST_CHECK(car.GetGear() == Gear::NEUTRAL);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
 
 	BOOST_AUTO_TEST_SUITE(if_car_engine_is_turned_off)
 		BOOST_AUTO_TEST_CASE(engine_can_be_turned_on)
@@ -60,37 +64,37 @@ BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 	};
 
 	BOOST_FIXTURE_TEST_SUITE(when_engine_turned_on, when_engine_turned_on_)
-		BOOST_AUTO_TEST_CASE(car_speed_is_zero_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(speed_is_zero_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.GetSpeed() == 0);
 		}
 
-		BOOST_AUTO_TEST_CASE(car_direction_is_NONE_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(direction_is_NONE_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.GetDirection() == Direction::NONE);
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_is_NEUTRAL_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(gear_is_NEUTRAL_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.GetGear() == Gear::NEUTRAL);
 		}
 
-		BOOST_AUTO_TEST_CASE(car_engine_can_be_turned_off_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(engine_can_be_turned_off_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.TurnOffEngine());
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_can_be_changed_to_REVERSE_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(gear_can_be_changed_to_REVERSE_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.SetGear(Gear::REVERSE));
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_can_be_changed_to_FIRST_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(gear_can_be_changed_to_FIRST_immediately_after_turning_on)
 		{
 			BOOST_CHECK(car.SetGear(Gear::FIRST));
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_cant_be_changed_for_more_than_FIRST_gear_value_immediately_after_turning_on)
+		BOOST_AUTO_TEST_CASE(gear_cant_be_changed_for_more_than_FIRST_gear_value_immediately_after_turning_on)
 		{
 			BOOST_CHECK(!car.SetGear(Gear::SECOND));
 		}
@@ -110,25 +114,25 @@ BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 			BOOST_CHECK(!car.SetSpeed(20));
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_can_be_changed_to_REVERSE_if_speed_is_zero_and_gear_is_NEUTRAL)
+		BOOST_AUTO_TEST_CASE(gear_can_be_changed_to_REVERSE_if_speed_is_zero_and_gear_is_NEUTRAL)
 		{
 			BOOST_CHECK(car.GetSpeed() == 0);
 			BOOST_CHECK(car.SetGear(Gear::REVERSE));
 		}
 
-		BOOST_AUTO_TEST_CASE(car_gear_can_be_changed_to_FIRST_from_NEUTRAL)
+		BOOST_AUTO_TEST_CASE(gear_can_be_changed_to_FIRST_from_NEUTRAL)
 		{
 			BOOST_CHECK(car.SetGear(Gear::FIRST));
 		}
 
-		BOOST_AUTO_TEST_CASE(car_accelerate_if_gear_is_not_NEUTRAL)
+		BOOST_AUTO_TEST_CASE(accelerate_if_gear_is_not_NEUTRAL)
 		{
 			BOOST_CHECK(car.SetGear(Gear::FIRST));
 			BOOST_CHECK(car.SetSpeed(30));
 			BOOST_CHECK(car.GetSpeed() == 30);
 		}
 
-		BOOST_AUTO_TEST_CASE(car_slow_down_if_gear_is_not_NEUTRAL)
+		BOOST_AUTO_TEST_CASE(slow_down_if_gear_is_not_NEUTRAL)
 		{
 			BOOST_CHECK(car.SetGear(Gear::FIRST));
 			BOOST_CHECK(car.SetSpeed(30));
@@ -137,7 +141,7 @@ BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 			BOOST_CHECK(car.GetSpeed() == 10);
 		}
 
-		BOOST_AUTO_TEST_CASE(car_slow_down_if_gear_NEUTRAL)
+		BOOST_AUTO_TEST_CASE(slow_down_if_gear_NEUTRAL)
 		{
 			BOOST_CHECK(car.SetGear(Gear::FIRST));
 			BOOST_CHECK(car.SetSpeed(30));
