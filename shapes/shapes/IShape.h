69f88e769efc88222824d7944ecad26775bc2121
AnnVita <kublitskayavd@yandex.ru>
2016-12-04 16:50:12 +0300

    Shapes init commit
    первый коммит и нтерфейс IShape

--- shapes/shapes/IShape.h
+++ shapes/shapes/IShape.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "stdafx.h"
+
+class IShape
+{
+public:
+	IShape() = default;
+	IShape(std::string const & type);
+	virtual ~IShape() = default;
+
+	virtual double GetArea() const = 0;
+	virtual double GetPerimeter() const = 0;
+	virtual std::string GetOutlineColor() const = 0;
+	std::string ToString() const;
+
+protected:
+	virtual void AppendProperties(std::ostream & strm) const = 0;
+
+private:
+	std::string m_type;
+};
