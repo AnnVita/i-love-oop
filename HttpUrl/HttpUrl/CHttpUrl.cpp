8e8bc822cb1f41334645c153070adbddadc3c7bf
AnnVita <kublitskayavd@yandex.ru>
2016-12-13 20:12:52 +0300

    init commit + побаловалась с регулярными выражениями
    Составлены выражения для парсинга URL и проверки на валидность. Эта
    штука умеет выделять протокол, домен, порт(если указан в URL), файл с
    адресом

---
+++ HttpUrl/HttpUrl/CHttpUrl.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+//#include "CHttpUrl.h"
+//
+//CHttpUrl::CHttpUrl(std::string const & url)
+//{
+//	boost::regex xRegEx("(\\w|\\d)+");
+//	boost::regex_match(url, xRegEx);
+//}
+//
+//CHttpUrl::CHttpUrl(std::string const & domain, std::string const & document, Protocol = HTTP, unsigned short port = 80)
+//{
+//
+//}
---
+++ HttpUrl/HttpUrl/CHttpUrl.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "stdafx.h"
+//
+//enum Protocol
+//{
+//	HTTP,
+//	HTTPS
+//};
+//
+//class CHttpUrl
+//{
+//public:
+//	CHttpUrl(std::string const & url);
+//	CHttpUrl(
+//		std::string const & domain,
+//		std::string const & document,
+//		Protocol = HTTP,
+//		unsigned short port = 80);
+//
+//	std::string GetURL() const;
+//	std::string GetDomain() const;
+//	std::string GetDocument()const;
+//	Protocol GetProtocol()const;
+//	unsigned short GetPort()const;
+//private:
+//	std::string m_domain,
+//	std::string m_document,
+//	Protocol m_protocol,
+//	unsigned short port,
+//};
