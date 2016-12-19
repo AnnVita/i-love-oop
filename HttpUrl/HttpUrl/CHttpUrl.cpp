e791eeb18f561c413d5bda19fa56a1df2ade6178
AnnVita <kublitskayavd@yandex.ru>
2016-12-19 18:43:50 +0300

    Приведена в порядок константность методов

--- HttpUrl/HttpUrl/CHttpUrl.cpp
+++ HttpUrl/HttpUrl/CHttpUrl.cpp
@@ -5,7 +5,7 @@
 CHttpUrl::CHttpUrl(std::string const & url)
 {
 	std::pair<std::string, std::string> parsedUrl = ParseUrl(url);
-	m_protocol = from_string(ParseProtocol(parsedUrl.first));
+	m_protocol = StringToProtocol(ParseProtocol(parsedUrl.first));
 	std::pair<std::string, std::string> parsedAddress = ParseAddress(parsedUrl.second);
 	std::pair<std::string, unsigned short> parsedDomain = ParseDomain(parsedAddress.first);
 	m_domain = parsedDomain.first;
@@ -22,52 +22,52 @@ CHttpUrl::CHttpUrl(const std::string & domain, const std::string & file, Protoco
 	m_file = ParseFile(file);
 };
 
-const std::pair<std::string, std::string> CHttpUrl::ParseUrl(const std::string & url)
+std::pair<std::string, std::string> CHttpUrl::ParseUrl(const std::string & url) const
 {
 	boost::regex urlRegEx("(\\w+)://(\\S+)");
 	boost::smatch urlResults;
 	if (!boost::regex_match(url, urlResults, urlRegEx))
-		throw CUrlParsingError("invalid string");
+		throw CUrlParsingError("INVALID_STRING");
 	return std::make_pair<std::string, std::string>(urlResults[1], urlResults[2]);
 }
 
-const std::string CHttpUrl::ParseProtocol(const std::string & protocol)
+std::string CHttpUrl::ParseProtocol(const std::string & protocol) const
 {
 	boost::regex protocolRegEx("(?i)https?");
 	if (!boost::regex_match(protocol, protocolRegEx))
-		throw CUrlParsingError("invalid protocol");
+		throw CUrlParsingError("INVALID_PROTOCOL");
 	return boost::algorithm::to_lower_copy(protocol);
 }
 
-const std::string CHttpUrl::to_string(Protocol protocolValue)
+std::string CHttpUrl::ProtocolToString(Protocol protocolValue) const
 {
 	return (protocolValue == Protocol::HTTPS) ? "https" : "http";
 }
 
-const Protocol CHttpUrl::from_string(const std::string & value)
+Protocol CHttpUrl::StringToProtocol(const std::string & value) const
 {
 	return (value == "https") ? Protocol::HTTPS : Protocol::HTTP;
 }
 
-const std::pair<std::string, std::string> CHttpUrl::ParseAddress(const std::string & address)
+std::pair<std::string, std::string> CHttpUrl::ParseAddress(const std::string & address) const
 {
 	boost::regex addressRegEx("([^\\s/]+)(/\\S*)?");
 	boost::smatch addressResults;
 	if (!boost::regex_match(address, addressResults, addressRegEx))
-		throw CUrlParsingError("invalid address");
+		throw CUrlParsingError("INVALID_ADDRESS");
 	return std::make_pair<std::string, std::string>(addressResults[1], addressResults[2]);
 }
 
-const std::pair<std::string, unsigned short> CHttpUrl::ParseDomain(const std::string & domain)
+std::pair<std::string, unsigned short> CHttpUrl::ParseDomain(const std::string & domain) const
 {
 	boost::regex domainRegEx("([^\\s/:]+)+(:(\\d+))?");
 	boost::smatch domainResults;
 	unsigned short port;
 	if (!boost::regex_match(domain, domainResults, domainRegEx))
-		throw CUrlParsingError("invalid domain");
+		throw CUrlParsingError("INVALID_DOMAIN");
 	if (domainResults[2] == "")
 	{
-		port = (m_protocol == Protocol::HTTP) ? 80 : 443;
+		port = static_cast<unsigned short>(m_protocol);
 	}
 	else
 	{
@@ -76,17 +76,17 @@ const std::pair<std::string, unsigned short> CHttpUrl::ParseDomain(const std::st
 	return std::pair<std::string, unsigned short>(domainResults[1], port);
 }
 
-const std::string CHttpUrl::ParseFile(const std::string & file)
+std::string CHttpUrl::ParseFile(const std::string & file) const
 {
 	boost::regex fileRegEx("/?([^\\s]+)?");
 	if(!boost::regex_match(file, fileRegEx))
-		throw CUrlParsingError("invalid file name");
+		throw CUrlParsingError("INVALID_FILE_NAME");
 	return (file[0] == '/') ? file : "/" + file;
 }
 
 std::string CHttpUrl::GetURL() const
 {
-	std::string protocolString = (m_protocol == Protocol::HTTPS) ? "https" : "http";
+	std::string protocolString = ProtocolToString(m_protocol)/*(m_protocol == Protocol::HTTPS) ? "https" : "http"*/;
 	return protocolString + "://" + m_domain + m_file;
 }
 
--- HttpUrl/HttpUrl/CHttpUrl.h
+++ HttpUrl/HttpUrl/CHttpUrl.h
@@ -3,8 +3,8 @@
 
 enum Protocol
 {
-	HTTP,
-	HTTPS
+	HTTP = 80,
+	HTTPS = 443
 };
 
 class CHttpUrl
@@ -13,6 +13,7 @@ public:
 	CHttpUrl(std::string const & url);
 	CHttpUrl(const std::string & domain, const std::string & file, Protocol protocol, unsigned short port);
 
+
 	std::string GetURL() const;
 	std::string GetDomain() const;
 	std::string GetFile() const;
@@ -20,13 +21,13 @@ public:
 	unsigned short GetPort() const;
 
 private:
-	const std::pair<std::string, std::string> ParseUrl(const std::string & url);
-	const std::string ParseProtocol(const std::string & protocol);
-	const std::pair<std::string, std::string> ParseAddress(const std::string & address);
-	const std::pair<std::string, unsigned short> ParseDomain(const std::string & domain);
-	const std::string ParseFile(const std::string & file);
-	const std::string to_string(Protocol protocolValue);
-	const Protocol from_string(const std::string & value);
+	std::pair<std::string, std::string> ParseUrl(const std::string & url) const;
+	std::string ParseProtocol(const std::string & protocol) const;
+	std::pair<std::string, std::string> ParseAddress(const std::string & address) const;
+	std::pair<std::string, unsigned short> ParseDomain(const std::string & domain) const;
+	std::string ParseFile(const std::string & file) const;
+	std::string ProtocolToString(Protocol protocolValue) const;
+	Protocol StringToProtocol(const std::string & value) const;
 
 	std::string m_domain, m_file;
 	Protocol m_protocol;
